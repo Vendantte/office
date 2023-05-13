d205666c47175a1804c3b931341116c492ad7390
Vendantte <133399500+Vendantte@users.noreply.github.com>
2023-05-13 16:30:03 +0700

    Commit

---
+++ data.js
@@ -0,0 +1,293 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Директор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.1445980563042113,
+        "pitch": -0.012320663649649788,
+        "fov": 0.947459034658295
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.32061106152890595,
+          "pitch": 0.09697650023686144,
+          "rotation": 10.995574287564278,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Центр",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.06115922623256331,
+        "pitch": -0.0009557500461134794,
+        "fov": 0.947459034658295
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.35992136018795584,
+          "pitch": 0.06525122771791025,
+          "rotation": 1.5707963267948966,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.1261905814514783,
+          "pitch": 0.3274749580024885,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.101914609852493,
+          "pitch": 0.13400601921836497,
+          "rotation": 1.5707963267948966,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.7899888281305216,
+          "pitch": 0.13214914397989475,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 2.8151101031108645,
+          "pitch": 0.12133946179371158,
+          "rotation": 4.71238898038469,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Бухгалтерия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -1.5488214784278576,
+        "pitch": 0.009133273657877083,
+        "fov": 0.947459034658295
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23592396832877327,
+          "pitch": 0.2577870587545039,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.9036188256651272,
+          "pitch": 0.14616844067682244,
+          "rotation": 7.0685834705770345,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Входная зона",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.0449785625831218,
+        "pitch": 0.01657697982162709,
+        "fov": 1.046801451353858
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5261975143318782,
+          "pitch": 0.1581439437890051,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.965165041498322,
+          "pitch": 0.14488125457765832,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.09463574464948543,
+          "pitch": 0.15072381856171901,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.8199199113452948,
+          "pitch": 0.22020037508851864,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.06847786220068386,
+        "pitch": 0.0025627187712302657,
+        "fov": 1.075843026861147
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.001177183497262,
+          "pitch": 0.11840168788674887,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "Менеджер",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.05586029409888127,
+        "pitch": 0.01450454423631875,
+        "fov": 0.947459034658295
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.024163197038028272,
+          "pitch": 0.17261697964881328,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 0.7844395814044649,
+          "pitch": 0.13749435499838114,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Офис",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
